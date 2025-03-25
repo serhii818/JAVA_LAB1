65a9eae93f046c7004ac2970e63936779b9a4fb5
Konrad Kasza <69155813+KonradKasza@users.noreply.github.com>
2025-03-25 18:09:27 +0100

    onp
    ONP

---
+++ src/IllegalNegativeNumberInOperation.java
@@ -0,0 +1,11 @@
+package ONP;
+
+public class IllegalNegativeNumberInOperation extends RuntimeException {
+    //możliwość przekazywania w konstruktorze opisu błędu
+    public IllegalNegativeNumberInOperation() {
+      super("Nie można wykonać operacji z liczby ujemnej!");
+    }
+    public IllegalNegativeNumberInOperation(String message){
+        super(message);
+    }
+  }
